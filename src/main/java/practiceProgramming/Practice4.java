887fb5085a43c16946c425b69f2e4fc08f7fada8
Yash Rajpal <yashrajpal@Yashs-MacBook-Pro.local>
2021-10-15 16:19:42 +0530

    minor changes - 15th Oct

--- src/main/java/practiceProgramming/Practice4.java
+++ src/main/java/practiceProgramming/Practice4.java
@@ -4,7 +4,14 @@ public class Practice4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		String sentence="Im going to be the most badass engineer soon";
+		
+		String words[]=sentence.split(" ");
+		
+		
+		
+		
 	}
 
 }
