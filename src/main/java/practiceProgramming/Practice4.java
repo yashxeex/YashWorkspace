00d52eb31e91238c4e1d3e5f7298d9c670a2d6f1
Yash Rajpal <yashrajpal1995@gmail.com>
2021-10-15 18:53:41 +0530

    Wtf

--- src/main/java/practiceProgramming/Practice4.java
+++ src/main/java/practiceProgramming/Practice4.java
@@ -5,13 +5,31 @@ public class Practice4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String sentence="Im going to be the most badass engineer soon";
+		String sentence="big black bug bit a big black dog on his big black nose";
 		
 		String words[]=sentence.split(" ");
 		
+		int count;
 		
+		for(int i=0;i<words.length;i++) { 
+			
+			count=1;
+			for(int j=i+1;j<words.length;j++) { 
+				
+				if(words[i].equals(words[j])) { 
+					
+					words[j]="0";
+					count++;
+					
+				}
+			}
 		
-		
+			if(count>1 && words[i]!="0") { 
+				
+				System.out.println(words[i]);
+				
+			}
+		}
 	}
 
 }
